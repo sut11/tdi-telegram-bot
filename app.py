a0794263ea6e310915806808b9a14a345b20e229
sut11 <nguyenthanhtugm@gmail.com>
2025-12-06 07:37:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from datetime import datetime
 app = Flask(__name__)
 
 # ===== CẤU HÌNH =====
+import os
 TELEGRAM_BOT_TOKEN = os.environ.get('8237221009:AAGzLVpVnPcZUzrrjtS60XA38JJywsRNMRk')  # Lấy từ @BotFather
 TELEGRAM_CHAT_ID = os.environ.get('5690514116')     # Lấy từ @userinfobot
 
